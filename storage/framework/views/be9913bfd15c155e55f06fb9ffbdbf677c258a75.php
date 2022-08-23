71231123370a184accbb8501a59c5136e788f087
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-08-22 22:29:28 -0300

    laravel - movido arquivos do Laravel para o raiz do projeto

---
+++ storage/framework/views/be9913bfd15c155e55f06fb9ffbdbf677c258a75.php
@@ -0,0 +1,17 @@
+<head>
+    <title><?php echo $__env->yieldContent("titulo"); ?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
+</head>
+
+<header id="header" style="background: white;">
+    <h2>Cabeçalho da Igreja</h2>
+</header>
+<main style="background: grey;">
+    <?php echo $__env->yieldContent("corpo"); ?>
+</main>
+
+<footer id="rodape" style="background: blue;">
+    <h2>Rodapé da Igreja</h2>
+</footer><?php /**PATH E:\Users\Acer\Desktop\hub\ProjetoADAD\resources\views/layouts/template.blade.php ENDPATH**/ ?>
