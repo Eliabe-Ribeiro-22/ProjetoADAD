89e92d5b7315d7c47f7e8166198a5e10f6254e39
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-05 17:35:46 -0300

    php - web, formIgreja - criado formulário para contato
    Neste commit for criado um formulário para o visitante entrar em contato com a IEAD Campo Belo do Sul.

--- resources/views/igreja/formIgreja.blade.php
+++ resources/views/igreja/formIgreja.blade.php
@@ -3,16 +3,19 @@
 @section("titulo", "Formulario para contato")
 
 @section("corpo")
-<form method="post">
+<form action="" method="post">
     <!-- 
         Form com:
             - Nome
             - telefone
             - Mensagem que o visitante irá enviar 
     -->
-    <h1>Formulario</h1>
+    <h1>Entre em contato conosco:</h1>
+    <label>Seu nome:</label>
     <input type="text" name="nome" placeholder="Digite seu nome"></input>
+    <label>Seu número de telefone:</label>
     <input type="text" name="nome" placeholder="Digite seu telefone"></input>
+    <label>Mensagem:</label>
     <textarea rows="5" cols="33" placeholder="Digite a mensagem"></textarea>
     <input type="submit" value="Enviar">
 </form>
