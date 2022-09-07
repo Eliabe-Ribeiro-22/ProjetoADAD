d030c7ca0b7a7fb3d5c010cbbbee82b2a43f7e1c
Alessandro Geras <96837746+AlessandroGeras@users.noreply.github.com>
2022-09-06 23:46:05 -0300

    AdadController - Criado função para envio de email

--- app/Http/Controllers/AdadController.php
+++ app/Http/Controllers/AdadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use Illuminate\Console\View\Components\Alert;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class AdadController extends Controller
 {
@@ -26,11 +27,23 @@ class AdadController extends Controller
 
     public function sendForm()
     {
-        // enviar form de contato Igreja por gmail
-        try {
-            echo "Mensagem enviada com sucesso!";
-        } catch (Exception $e) {
-            return $e->getMessage();
+        $credentials = $request->validate([            
+            'nome' => ['required'],
+            'telefone' => ['required'],
+            'mensagem' => ['required'],
+        ]);
+
+        if ($credentials) {
+            Mail::send('email.sendMail', function ($message) use ($request) {
+                    $message->to('adcbsul@gmail.com');
+                    $message->from('adcbsul@gmail.com', 'ADCBSUL');
+                    $message->subject('Mensagem via formulário');
+                });
+
+                return redirect('/')->with("msg", "O formulário foi enviado com sucesso");
+            } else {
+                return redirect('/')->with('message', "Falha ao enviar o formulário. Tente mais tarde");
+            }
         }
     }
 }
