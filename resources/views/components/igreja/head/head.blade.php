1045dd98a8355bc412bddf1b09f09f04c780d131
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-10 11:28:26 -0300

    php - stylesIgreja, header, footer - criado header, footer do site
    Criado arquivo de estilização da Igreja, com as devidas componentizações.\nEstilização do header e footer do site

--- resources/views/components/igreja/head/head.blade.php
+++ resources/views/components/igreja/head/head.blade.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta charset="utf-8" />
 <link rel="stylesheet" type="text/css" href="assets/css/igreja/stylesIgreja.css">
+
 <title>@yield("titulo")</title>
--- resources/views/components/igreja/nossas-reunioes/nossas-reunioes_main.blade.php
+++ resources/views/components/igreja/nossas-reunioes/nossas-reunioes_main.blade.php
@@ -1,11 +1,11 @@
 {{-- Main do nossas-reunioes --}}
 <main>
-    <h1>Nossas Reuniões</h1><br />
+    <h1>Nossas Reuniões</h1>
 
-    <h2>Cultos</h2><br />
+    <h2>Cultos</h2>
 
     <h3>Noite da Vitória</h3>
-    <p>Quintas-feiras às 19:15hs</p><br />
+    <p>Quintas-feiras às 19:15hs</p>
 
     <h3>Noite da Família</h3>
     <p>Domingos às 19:00hs</p>
