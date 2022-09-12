aa44321a662a2c95e913cf5b63f1f151de88728c
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2022-09-12 19:58:57 -0300

    php - sendMail - Refatorado código de envio de email

--- resources/views/email/sendMail.blade.php
+++ resources/views/email/sendMail.blade.php
@@ -1,5 +1,7 @@
 <h1>{{$nome}} enviou uma mensagem para IEAD Campo Belo do Sul</h1>
-Nome: {{ $nome }} <br />
-Telefone: {{ $telefone }} <br />
+<h2>Nome</h2>>
+{{ $nome }} <br>
+<h2>Telefone</h2>
+{{ $telefone }} <br>
 <h2>Mensagem:</h2>
 <p>{{ $mensagem }}</p>
