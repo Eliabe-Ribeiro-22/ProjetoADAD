9dbbfb1f65a8454c52f1e2c2ec095618f5d90439
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-23 16:00:35 -0300

    php - area-restrita.php - refatorado código

--- resources/views/adad/area-restrita.blade.php
+++ resources/views/adad/area-restrita.blade.php
@@ -11,7 +11,7 @@ $title = 'Alterar aluno';
 
 <head>
     <meta charset="utf-8" />
-	<link rel="stylesheet" href="/assets/css/adad/login.css">
+	<link rel="stylesheet" href="/assets/css/adad/alunos.css">
     <!-- link rel="stylesheet" type="text/css" href="assets/css/igreja/styles.css" -->
     <title>{{$title}}</title>
 </head>
@@ -294,7 +294,7 @@ $title = 'Alterar aluno';
             @if ($alterar)
             {{-- Cancelar Alterações --}}
             <div class="flex items-center justify-end mt-4">
-                <a href="{{ route('AlunosCreate') }}" class="underline mx-auto text-sm transition" >
+                <a href="{{ route('aluno_create') }}" class="underline mx-auto text-sm transition" >
 				Cancelar Alterações
 				</a>
             </div>
