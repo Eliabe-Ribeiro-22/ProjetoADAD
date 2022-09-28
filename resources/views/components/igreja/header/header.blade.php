dae595b11ceeec567bce621e0c8cf3a2d865332c
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-28 16:51:39 -0300

    css - estilizando nav_bar

--- resources/views/components/igreja/header/header.blade.php
+++ resources/views/components/igreja/header/header.blade.php
@@ -1,38 +1,48 @@
-{{-- Header do templateIgreja --}}
-<header>
-    <nav>
-        <div id="logo">
-            {{-- <img src="assets/img/igreja/logoIgreja.jpeg" alt="logo da Igreja" id="t"> --}}
-            <img src="assets/img/igreja/logoIgreja.png" alt="logo da Igreja" id="logo-igreja">
+{{-- nav_bar do templateIgreja --}}
+<nav>
+    <div id="nav_logo">
+        <img src="/img/adad_logo.png" alt="logo-adad" id="logo-adad">
 
-            <a id="nomeIgreja">
-                ASSEMBLEIA DE DEUS
-                <br />
-                CAMPO BELO DO SUL
-            </a>
-        </div>
+        <img src="/assets/img/igreja/logoIgreja.png" alt="logo da Igreja" id="logo-igreja">
+    </div>
 
-        <div id="menu">
-            <ul id="ul">
-                <li class="li"><a href="{{ route('index.page') }}" class="btn">Início</a></li>
+    <div id="nav_menu">
+        <ul id="menu_button_ul">
+            <li class="menu_button_li">
+                <a href="{{ route('index.page') }}" class="menu_button_item">
+                    Início
+                </a>
+            </li>
 
-                <li class="li">
-                    <a href="{{ route('institucional_create') }}" class="btn" id="btnInstitucional">
-                        Institucional
-                    </a>
-                </li>
+            <li class="menu_button_li">
+                <a href="{{ route('institucional_create') }}" class="menu_button_item" id="btnInstitucional">
+                    Institucional
+                </a>
+            </li>
 
-                <li class="li"><a href="{{ route('form_create') }}" class="btn">Contato</a></li>
-                <li class="li"><a href="{{ route('projetos_create') }}" class="btn">Nossos Projetos</a></li>
+            <li class="menu_button_li">
+                <a href="{{ route('form_create') }}" class="menu_button_item">
+                    Contato
+                </a>
+            </li>
 
-                <li class="li">
-                    <a href="{{ route('nossas_reunioes_create') }}" class="btn" id="btnReunioes">
-                        Nossas Reuniões
-                    </a>
-                </li>
+            <li class="menu_button_li">
+                <a href="{{ route('projetos_create') }}" class="menu_button_item" id="btnProjetos">
+                    Nossos Projetos
+                </a>
+            </li>
 
-                <li class="li"><a href="{{ route('aluno_create') }}" class="btn">Área Restrita</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
+            <li class="menu_button_li">
+                <a href="{{ route('nossas_reunioes_create') }}" class="menu_button_item" id="btnReunioes">
+                    Nossas Reuniões
+                </a>
+            </li>
+
+            <li class="menu_button_li">
+                <a href="{{ route('aluno_create') }}" class="menu_button_item" id="btnAreaRestrita">
+                    Área Restrita
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- resources/views/igreja/institucional.blade.php
+++ resources/views/igreja/institucional.blade.php
@@ -1,12 +1,8 @@
 {{-- Institucional --}}
-{{-- @extends('layouts.templateIgreja')
+@extends('layouts.templateIgreja')
 
 @section('titulo', 'Institucional')
 
-@section('corpo') --}}
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="/assets/css/igreja/styles.css">
-
-<x-igreja.institucional.institucional_main />
-{{-- @endsection --}}
+@section('corpo')
+    <x-igreja.institucional.institucional_main />
+@endsection
