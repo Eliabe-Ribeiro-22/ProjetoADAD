b7e8d2128b7189bcc288b09eacf9614eef768b6b
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-28 02:17:51 -0300

    css - institucional - mobile - ok

--- resources/views/igreja/institucional.blade.php
+++ resources/views/igreja/institucional.blade.php
@@ -1,8 +1,12 @@
 {{-- Institucional --}}
-@extends('layouts.templateIgreja')
+{{-- @extends('layouts.templateIgreja')
 
 @section('titulo', 'Institucional')
 
-@section('corpo')
-    <x-igreja.institucional.institucional_main />
-@endsection
+@section('corpo') --}}
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="/assets/css/igreja/styles.css">
+
+<x-igreja.institucional.institucional_main />
+{{-- @endsection --}}
