631761d0c8dffb15c0b0052b0e648eb5553a0f14
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-09-28 01:40:11 -0300

    css - Iniciando responsividade - 29/09

--- resources/views/components/igreja/index/index_main.blade.php
+++ resources/views/components/igreja/index/index_main.blade.php
@@ -1,35 +1,40 @@
 {{-- Main do Index --}}
 @section('corpo')
-    <!-- Section Boas-vindas ao Site -->
-    <section>
-        <div class="boas-vindas">
-            <!-- To Vini: Criar aqui "Carrossel" -->
-            <img src="#" alt="Imagem de Boas-Vindas">
-            
-            <!-- Video para ser colocado como fundo -->
-            <video controls loop muted>
-                <source src="assets/videos/video.mp4" type="video/mp4">
-                    Seu navegador não tem suporte para esse vídeo
-            </video>
-            
-            <h1>Bem-vindo a IEAD Campo Belo do Sul</h1>
-        </div>
-    </section>
+<!-- Section Boas-vindas ao Site -->
+<section>
+    <div class="boas-vindas">
+        <!-- To Vini: Criar aqui "Carrossel" -->
+        <img src="#" alt="Imagem de Boas-Vindas">
 
-    <!-- Seção do Versículo Diário -->
-    <section>
-        <h1>Versículo Diário</h1>
-        <p>O Senhor é o meu pastor e nada me faltará!</p>
-        <p>Salmos 23.1</p>
-    </section>
+        <!-- Video para ser colocado como fundo -->
+        <video controls loop muted>
+            <source src="assets/videos/video.mp4" type="video/mp4">
+            Seu navegador não tem suporte para esse vídeo
+        </video>
 
-    <!-- Section Projetos -->
-    <section>
-        <x-igreja.projetos.projetos_main />
-    </section>
+        <h1>Bem-vindo a IEAD Campo Belo do Sul</h1>
+    </div>
+</section>
 
-    <!-- Section Nossas Reuniões -->
-    <section>
-        <x-igreja.nossas-reunioes.nossas-reunioes_main />
-    </section>
+<!-- Seção do Versículo Diário -->
+<section>
+    <h1>Versículo diário</h1>
+    <?php
+    echo '<p>', $dailyVerse['text'], '.', '</p>';
+    echo '<p class="verse" id="b">',
+    $dailyVerse['book']['name'], ' ',
+    $dailyVerse['chapter'], '.', $dailyVerse['number'],
+    '</p>';
+    ?>
+</section>
+
+<!-- Section Projetos -->
+<section>
+    <x-igreja.projetos.projetos_main />
+</section>
+
+<!-- Section Nossas Reuniões -->
+<section>
+    <x-igreja.nossas-reunioes.nossas-reunioes_main />
+</section>
 @endsection
