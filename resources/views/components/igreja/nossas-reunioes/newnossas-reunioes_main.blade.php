ccbdbf06c92c2caab13aa3789af6d76a16bc74f3
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-02 20:00:29 -0300

    css - Responsividade Nossas Reuniões

--- resources/views/components/igreja/nossas-reunioes/newnossas-reunioes_main.blade.php
+++ resources/views/components/igreja/nossas-reunioes/newnossas-reunioes_main.blade.php
@@ -6,9 +6,15 @@
 
 <div class="square">
     <div>
-        <div id="padding-bottom-footer">
+        <div id="pb-footer-nossas-reunioes">
             <p class="text bg-div color-primary" id="text-institucional">
-               ok
+                <span style="color:white;">ok</span>
+                Domingos às 19:00 hs
+                <a href="a">ok</a>
+                <br>
+                Segundas-feiras às 19:00 hs
+                <br>
+                Quintas-Feiras às 19:15 hs
             </p>
         </div>
     </div>
