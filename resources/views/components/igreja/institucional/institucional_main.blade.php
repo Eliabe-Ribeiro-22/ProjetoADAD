eff3cd80b531ee23659d7c494db9f0023539c9ed
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-02 01:31:10 -0300

    css - página Institucional

--- resources/views/components/igreja/institucional/institucional_main.blade.php
+++ resources/views/components/igreja/institucional/institucional_main.blade.php
@@ -5,9 +5,9 @@
 
 
 <div class="square">
-    <div id="textPosition">
+    <div>
         <div>
-            <p class="text bg-div" id="text-institucional">
+            <p class="text bg-div color-primary" id="text-institucional">
                 Instituição religiosa sem fins lucrativos que visa o bem social e
                 espiritual de toda a comunidade.
             </p>
