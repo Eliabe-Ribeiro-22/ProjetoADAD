e272812e806cd247a6f6ba248dc4ca6cdb6faa2c
Vinícius Jung <viniciusjung@outlook.com>
2022-10-03 15:43:21 -0300

    Toastr - Criado script em Javascript para Toastr

---
+++ public/assets/js/toastr.js
@@ -0,0 +1,27 @@
+// Ativar Toaster ao tentar logar
+const loading = (messagem) => {
+toastr.options = {
+    progressBar: true,
+    timeOut: "60000",
+};
+toastr["info"](messagem);
+};
+
+
+// Ativar Toastr (Mensagem de sucesso)
+const success = (mensagem) => {
+    toastr.options = {
+        progressBar: true,
+        timeOut: "5000",
+    };
+    toastr["success"](mensagem);
+};
+
+// Ativar Toastr (Mensagem  de erro)
+const error = (mensagem) => {
+    toastr.options = {
+        progressBar: true,
+        timeOut: "5000",
+    };
+    toastr["error"](mensagem);
+};
