e857b1917539b0268dd9ba6b1c80f1eaba8c2914
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-05 02:27:04 -0300

    css - Responsividade Cadastro de Alunos

--- resources/views/adad/area-restrita.blade.php
+++ resources/views/adad/area-restrita.blade.php
@@ -310,7 +310,7 @@ $title = 'Alterar aluno';
     <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             <div id="table" style="margin: 0 auto; background:black">
-                <div style="display: flex; background:red">
+                <div class="line" style="display: flex; background:red">
                     <div class="th" id="nome">Nome</div>
                     <div class="th" id="idade">Idade</div>
                     <div class="th" id="serie">Série</div>
@@ -318,12 +318,12 @@ $title = 'Alterar aluno';
                     <div class="th" id="religiao">Religião</div>
                 </div>
                 
-                <div style="display: flex; background:green">
-                    <div class="th" id="nome">Nome</div>
-                    <div class="th" id="idade">Idade</div>
-                    <div class="th" id="serie">Série</div>
-                    <div class="th" id="cidade">Cidade</div>
-                    <div class="th" id="religiao">Religião</div>
+                <div class="line" style="display: flex; background:green;">
+                    <div class="th" id="nome">Eliabe da TI</div>
+                    <div class="th" id="idade">11</div>
+                    <div class="th" id="serie">Ensino Médio</div>
+                    <div class="th" id="cidade">CBS</div>
+                    <div class="th" id="religiao">Evangélico</div>
                 </div>
             </div>
             
