6547c178941e8a30842deb7970b6f059e2a8d34a
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-05 02:35:40 -0300

    css - Responsividade Cadastro de Alunos

---
+++ resources/views/adad/listar-aluno.blade.php
@@ -0,0 +1,24 @@
+<div class="font-sans text-gray-900 antialiased">
+    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div id="table" style="margin: 0 auto; background:black">
+                <div class="line" style="display: flex; background:red">
+                    <div class="th" id="nome">Nome</div>
+                    <div class="th" id="idade">Idade</div>
+                    <div class="th" id="serie">Série</div>
+                    <div class="th" id="cidade">Cidade</div>
+                    <div class="th" id="religiao">Religião</div>
+                </div>
+
+                <div class="line" style="display: flex; background:green;">
+                    <div class="th" id="nome">Eliabe da TI</div>
+                    <div class="th" id="idade">11</div>
+                    <div class="th" id="serie">Ensino Médio</div>
+                    <div class="th" id="cidade">CBS</div>
+                    <div class="th" id="religiao">Evangélico</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
