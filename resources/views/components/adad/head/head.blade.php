ecb20376d99a34dab2adf997195c2ffd74b8ceeb
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-07 23:02:02 -0300

    css - Refatoração de 404

--- resources/views/components/adad/head/head.blade.php
+++ resources/views/components/adad/head/head.blade.php
@@ -1,6 +1,7 @@
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta charset="utf-8" />
     <link rel="stylesheet" href="/assets/css/adad/alunos.css">
     <link rel="icon" type="image/x-icon" href="/adad.ico">
-    <title>@yield("titulo")</title>
+    <title>@yield('titulo')</title>
 </head>
