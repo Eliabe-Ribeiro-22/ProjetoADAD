db7c58dd58c75999674ffb3b76b364f6be2e3f05
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-07 22:39:50 -0300

    css - Responsividade de font-size - Projetos

--- resources/views/components/igreja/projetos/projetos_main.blade.php
+++ resources/views/components/igreja/projetos/projetos_main.blade.php
@@ -5,7 +5,7 @@
     <div>
         <div id="pb-footer-projetos">
             <div class="projeto">
-                <h2 class="subtitle-projeto" id="ok">Projeto de Prática de fé</h2>
+                <h2 class="subtitle-projeto">Projeto de Prática de fé</h2>
                 <ul class="ul-projeto">
                     <li>Realização de cultos com ministração de louvores e palavra</li>
                     <li>Realização de cultos com ensino da Palavra de Deus</li>
