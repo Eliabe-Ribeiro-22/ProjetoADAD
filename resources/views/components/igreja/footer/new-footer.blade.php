48565827800c72e8f7c78567af4f93e41841c747
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-12 19:56:45 -0300

    css - Responsividade do footer - site

--- resources/views/components/igreja/footer/new-footer.blade.php
+++ resources/views/components/igreja/footer/new-footer.blade.php
@@ -1,14 +1,14 @@
 {{-- Footer do templateIgreja --}}
 <div>
     <div>
-        <p class="text-center text-font-footer color-primary">Telefones:</p>
-        <p class="text-center text-font-footer color-primary"> (049) 3249-1036</p>
+        <p class="text-center text-font-footer size-footer color-primary">Telefones:</p>
+        <p class="text-center text-font-footer size-footer color-primary"> (049) 3249-1036</p>
     </div>
     <div id="telefone-whats">
         <div id="tel-whats-div">
             <img class="w-img" src="assets/img/igreja/whats.png" alt="Logo What's App" />
         </div>
-        <p class="text-center text-font-footer color-primary">+55 (049) 9 8847-7611</p>
+        <p class="text-center text-font-footer size-footer color-primary">+55 (049) 9 8847-7611</p>
     </div>
 
     <div id="rede-social">
@@ -24,7 +24,7 @@
 
 {{-- Endereco --}}
 <div id="endereco">
-    <p class="text-font-footer color-primary" id="text-endereco">
+    <p class="text-font-footer size-footer color-primary" id="text-endereco">
         Rua Gerônimo Xavier de Oliveira, nº 230, Centro, Campo Belo do Sul/SC, Brasil
     </p>
 </div>
