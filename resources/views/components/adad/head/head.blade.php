89a246f8a1425dd6d7ac5c7b7e56eca5cff38d9d
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-12 19:48:03 -0300

    css - Responsividade footer

--- resources/views/components/adad/head/head.blade.php
+++ resources/views/components/adad/head/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta charset="utf-8" />
 
-<link rel="stylesheet" href="/assets/css/adad/alunos.css">
+<link rel="stylesheet" href="/assets/css/adad/area-restrita/alunos.css">
 
 <link rel="icon" type="image/x-icon" href="/adad.ico">
 
--- resources/views/components/igreja/footer/new-footer.blade.php
+++ resources/views/components/igreja/footer/new-footer.blade.php
@@ -1,34 +1,30 @@
 {{-- Footer do templateIgreja --}}
 <div>
     <div>
-        <p class="text-center text-font-footer olor-white">Telefones:</p>
-        <p class="text-center text-font-footer olor-white"> (049) 3249-1036</p>
+        <p class="text-center text-font-footer color-primary">Telefones:</p>
+        <p class="text-center text-font-footer color-primary"> (049) 3249-1036</p>
     </div>
     <div id="telefone-whats">
         <div id="tel-whats-div">
             <img class="w-img" src="assets/img/igreja/whats.png" alt="Logo What's App" />
         </div>
-        <p class="text-center text-font-footer olor-white">+55 (049) 9 8847-7611</p>
+        <p class="text-center text-font-footer color-primary">+55 (049) 9 8847-7611</p>
     </div>
 
-    <div>
-        <div href="https://www.facebook.com/deusprovera.campobelodosul">
-            <a href="https://www.facebook.com/deusprovera.campobelodosul" target="_blank">
-                <img class="" src="assets/img/igreja/logoFace.png" alt="Logo Facebook" />
-            </a>
-        </div>
+    <div id="rede-social">
+        <a class="div-img" href="https://www.facebook.com/deusprovera.campobelodosul" target="_blank">
+            <img class="w-img" src="assets/img/igreja/logoFace.png" alt="Logo Facebook" />
+        </a>
 
-        <div id="z">
-            <a href="mailto:adcbsul@gmail.com" target="_blank">
-                <img class="w-img" src="assets/img/igreja/logoGmail.png" alt="Logo Gmail" />
-            </a>
-        </div>
+        <a href="mailto:adcbsul@gmail.com" target="_blank">
+            <img class="w-img" src="assets/img/igreja/logoGmail.png" alt="Logo Gmail" />
+        </a>
     </div>
 </div>
 
 {{-- Endereco --}}
 <div id="endereco">
-    <p class="text-font-footer olor-white" id="text-endereco">
+    <p class="text-font-footer color-primary" id="text-endereco">
         Rua Gerônimo Xavier de Oliveira, nº 230, Centro, Campo Belo do Sul/SC, Brasil
     </p>
 </div>
