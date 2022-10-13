efcb542885e5ba47b975867ba0f85c334f4494da
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-13 00:49:57 -0300

    css - Refatoração do header do projeto - 13/10

--- resources/views/components/igreja/header/header.blade.php
+++ resources/views/components/igreja/header/header.blade.php
@@ -2,7 +2,7 @@
 <nav>
     <div id="nav_logo">
         <a href="{{ route('aluno_create') }}" target="_blank">
-            <img src="/img/adad_logo.png" alt="logo-adad" id="logo-adad">
+            <img src="/assets/img/igreja/adad_logo.png" alt="logo-adad" id="logo-adad">
         </a>
 
         <a href="{{ route('index.page') }}">
