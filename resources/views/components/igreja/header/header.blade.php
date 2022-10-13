46d87e3ed0550e5222439c10c7cd99cf5e52d6c4
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-13 00:47:21 -0300

    css - Refatoração do header do projeto - 13/10

--- resources/views/components/igreja/header/header.blade.php
+++ resources/views/components/igreja/header/header.blade.php
@@ -3,11 +3,11 @@
     <div id="nav_logo">
         <a href="{{ route('aluno_create') }}" target="_blank">
             <img src="/img/adad_logo.png" alt="logo-adad" id="logo-adad">
-        </a>  
-        
+        </a>
+
         <a href="{{ route('index.page') }}">
             <img src="/assets/img/igreja/logoIgreja.png" alt="logo da Igreja" id="logo-igreja">
-        </a>  
+        </a>
     </div>
 
     <div id="nav_menu">
--- resources/views/layouts/templateIgreja.blade.php
+++ resources/views/layouts/templateIgreja.blade.php
@@ -4,20 +4,16 @@
     <x-igreja.head.head />
 </head>
 
-<body class="bg-body">
-    <nav>
-        <div id="header">
-            <x-igreja.nav.logo />
-            <x-igreja.nav.menu-nav />
-            <x-igreja.nav.menu-mobile />
-        </div>
-    </nav>
+<body>
+    <header id="header">
+        <x-igreja.header.header />
+    </header>
 
-    {{-- <main class="bg-main">
+    <main class="bg-main">
         @yield('corpo')
-    </main> --}}
+    </main>
 
-    <footer>
+    <footer class="bg-body">
         <x-igreja.footer.footer />
     </footer>
 </body>
