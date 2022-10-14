765147f1deada2f41c125b545a961b4be1860080
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-14 01:49:38 -0300

    css - Refatoração de styles na Página Inicial

--- resources/views/components/igreja/projetos/projetos_main.blade.php
+++ resources/views/components/igreja/projetos/projetos_main.blade.php
@@ -1,49 +1,50 @@
-{{-- Main do projetos --}}
-<h1 class="titlePage" id="title-projetos">Nossos Projetos</h1>
+  {{-- Main do projetos --}}
+  <h1 class="titlePage" id="title-projetos">Nossos Projetos</h1>
 
-<div id="square">
-    <div>
-        <div id="pb-footer-projetos">
-            <div class="projeto">
-                <h2 class="subtitle-projeto">Projeto de Prática de fé</h2>
-                <ul class="ul-projeto">
-                    <li>Realização de cultos com ministração de louvores e palavra</li>
-                    <li>Realização de cultos com ensino da Palavra de Deus</li>
-                    <li>Realização de cultos evangelísticos com distribuição de literatura</li>
-                    <li>Realização de orações da fé por necessidades de cada família</li>
-                </ul>
-            </div>
+  <div>
+      <div class="projeto">
+          <h2 class="subtitle-projeto">Projeto de Prática de fé</h2>
+          <ul class="ul-projeto">
+              <li>Realização de cultos com ministração de louvores e palavra</li>
+              <li>Realização de cultos com ensino da Palavra de Deus</li>
+              <li>
+                  Realização de cultos evangelísticos com distribuição de literatura
+              </li>
+              <li>
+                  Realização de orações da fé por necessidades de cada família
+              </li>
+          </ul>
+      </div>
 
-            <div class="projeto">
-                <h2 class="subtitle-projeto">Projeto Educacional</h2>
-                <img src="/assets/img/igreja/adad_logo.png" id="image-adad">
-                <ul class="ul-projeto">
-                    <h3 id="ul-sigla-adad">ADAD = Adolescentes que Amam a Deus</h3>
-                    <li>
-                        Desenvolvem atividades com crianças, adolescentes e jovens visando
-                        fortalecer o Caráter de Cristo, valores, princípios morais e éticos, patriotismo, recreação,
-                        acampamentos, prática de escotismo e integração em busca de uma formação educacional
-                        por princípios.
-                    </li>
-                    <li>Realização de Palestras educativas de socialização para todas as faixas etárias.</li>
-                </ul>
-            </div>
+      <div class="projeto">
+          <h2 class="subtitle-projeto">Projeto Educacional</h2>
+          <img src="/assets/img/igreja/adad_logo.png" id="image-adad" />
+          <ul class="ul-projeto">
+              <h3 id="ul-sigla-adad">ADAD = Adolescentes que Amam a Deus</h3>
+              <li>
+                  Desenvolvem atividades com crianças, adolescentes e jovens visando
+                  fortalecer o Caráter de Cristo, valores, princípios morais e
+                  éticos, patriotismo, recreação, acampamentos, prática de escotismo
+                  e integração em busca de uma formação educacional por princípios.
+              </li>
+              <li>
+                  Realização de Palestras educativas de socialização para todas as
+                  faixas etárias.
+              </li>
+          </ul>
+      </div>
 
-            <div class="projeto">
-                <h2 class="subtitle-projeto">Projeto Social</h2>
-                <ul class="ul-projeto">
-                    <li>
-                        Distribuimos roupas usadas para pessoas carentes de nossa cidade e também nas localidades do
-                        interior do
-                        município
-                    </li>
-                    <li>
-                        Distribuimos cestas básicas de alimento para famílias carentes de nossa cidade e também nas
-                        localidades do
-                        interior
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
+      <div class="projeto">
+          <h2 class="subtitle-projeto">Projeto Social</h2>
+          <ul class="ul-projeto">
+              <li>
+                  Distribuimos roupas usadas para pessoas carentes de nossa cidade e
+                  também nas localidades do interior do município
+              </li>
+              <li>
+                  Distribuimos cestas básicas de alimento para famílias carentes de
+                  nossa cidade e também nas localidades do interior
+              </li>
+          </ul>
+      </div>
+  </div>
