fe93b6de2a4c76836506a06fff52082a377e3066
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-15 00:59:31 -0300

    css - nav - Responsividade Mobile First

--- resources/views/components/igreja/nav/logo.blade.php
+++ resources/views/components/igreja/nav/logo.blade.php
@@ -1,11 +1,11 @@
 {{-- Logos da Nav do templateIgreja --}}
 <div id="nav-logos">
     <div id="nav-logos-div">
-        <a id="nav-logo-link-adad" href="{{ route('aluno_create') }}" target="_blank">
+        <a class="nav-logo-link" id="nav-logo-link-adad" href="{{ route('aluno_create') }}" target="_blank">
             <img src="/assets/img/igreja/adad_logo.png" alt="logo do Adad" id="logo-adad" />
         </a>
     
-        <a id="nav-logo-link-igreja" href="{{ route('index.page') }}">
+        <a class="nav-logo-link" href="{{ route('index.page') }}">
             <img src="/assets/img/igreja/logoIgreja.png" alt="logo da Igreja" id="logo-igreja" />
         </a>
     </div>
