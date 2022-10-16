8e31ad8096ab51b3d564a0adc6fd1428077e6c6f
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-16 02:55:55 -0300

    Refatoração da Nav do Site

--- resources/views/components/igreja/nav/menu-mobile.blade.php
+++ resources/views/components/igreja/nav/menu-mobile.blade.php
@@ -1,7 +1,7 @@
 <div id="nav-mobile">
-    <a class="nav-mobile-item" href="#" onclick="abre_menu()">
+    <a class="menu-mobile-item" href="#" onclick="abre_menu()">
         &#9776;
     </a>
 
-    <a href="#" class="menu-mobile-item"> &#x2715;</a>
+    <a href="#" class="menu-mobile-item mobile-close"> &#x2715;</a>
 </div>
