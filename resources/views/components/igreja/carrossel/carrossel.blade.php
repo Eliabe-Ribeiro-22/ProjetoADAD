e24f21e1174aca52562050e951c97e70bd8a8e57
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-10-21 18:03:02 -0300

    html - Refatoração do carrossel

--- resources/views/components/igreja/carrossel/carrossel.blade.php
+++ resources/views/components/igreja/carrossel/carrossel.blade.php
@@ -14,17 +14,18 @@
     <button class="arrow-right control" aria-label="Next image">&#8594;</button>
     <div class="gallery-wrapper">
         <div class="gallery">
-            <img class="item" src="/assets/img/igreja/pc1.jpg" alt="Alunos fazendo um mosaico de 'Jesus te ama'.">
-            <img class="item current-item" src="/assets/img/igreja/pc2.jpg" alt="Fachada da igreja Assembleia de Deus.">
-            <img class="item" src="/assets/img/igreja/pc3.jpg"
+            <img class="item" src="/assets/img/igreja/carrossel-adad-grupo.jpg" alt="Alunos fazendo um mosaico de 'Jesus te ama'.">
+            <img class="item current-item" src="/assets/img/igreja/carrossel-templo.jpg" alt="Fachada da igreja Assembleia de Deus.">
+            <img class="item" src="/assets/img/igreja/carrossel-adad-faixa.jpg"
                 alt="Foto de alunos e professores com a faixa ADAD Campo Belo do Sul.">
-            <img class="item" src="/assets/img/igreja/pc4.jpeg" alt="Professores e tutores do programa ADAD.">
-            <img class="item" src="/assets/img/igreja/pc5.jpeg" alt="Imagem de uma pessoa orando com a bíblia de fundo.">
-            <img class="item" src="/assets/img/igreja/pc6.jpg" alt="Doação de roupas">
-            <img class="item" src="/assets/img/igreja/pc7.jpeg" alt="Alunos ADAD segurando bandeiras">
+            <img class="item" src="/assets/img/igreja/carrossel-lideranca.jpeg" alt="Professores e instrutores do programa ADAD.">
+            <img class="item" src="/assets/img/igreja/carrossel-biblia.jpeg" alt="Imagem de uma pessoa orando com a bíblia de fundo.">
+            <img class="item" src="/assets/img/igreja/carrossel-doacao-roupas.jpg" alt="Doação de roupas">
+            <img class="item" src="/assets/img/igreja/carrossel-adad-bandeiras.jpeg" alt="Alunos ADAD segurando bandeiras">
         </div>
     </div>
 </div>
+
 {{-- Script do carrossel --}}
 <script>
     slide();
