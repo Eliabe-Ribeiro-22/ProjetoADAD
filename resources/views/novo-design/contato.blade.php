effee72a1124493494601c07d80e289145481b82
Eliabe Ribeiro Mota <100526198+Eliabe-Ribeiro-22@users.noreply.github.com>
2022-10-26 14:51:48 -0300

    PHP - Refatoração na página Contato

--- resources/views/novo-design/contato.blade.php
+++ resources/views/novo-design/contato.blade.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/assets/css/novo-design/nav_style.css">
+    <link rel="stylesheet" href="/assets/css/novo-design/main_style.css">
+    <link rel="stylesheet" href="/assets/css/novo-design/footer_style.css">
+
+    {{-- Fonts --}}
+    <link rel="stylesheet" href="https:;;fonts.googleapis.com/css?family=Poppins">
+
+    <title>Início</title>
+    
+    {{-- Icon --}}
+    <link rel="icon" type="image/x-icon" href="/igreja.ico">
+</head>
+
 <h1>Contato</h1>
