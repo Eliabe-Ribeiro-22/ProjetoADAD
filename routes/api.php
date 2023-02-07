030f7a5fc7d9e9042fc330d56c43caa4e289a8c7
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2023-02-06 22:44:41 -0300

    PHP - Correções de bugs em rotas e Toastr(06/02)

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdadController;
 use App\Http\Controllers\IgrejaController;
 
+// Rotas relativas as páginas da igreja Campo Belo
+Route::get('/institucional', [IgrejaController::class, 'institutionalPg'])->name('institutional.page');
+Route::get('/contato', [IgrejaController::class, 'form_create'])->name('contact.page');
+Route::get('/projetos', [IgrejaController::class, 'projectsPg'])->name('projects.page');
+Route::get('/reunioes', [IgrejaController::class, 'reunionsPg'])->name('reunions.page');
+
+// Funções de Sistema de Login
+//  Login / Registrar (get)
+Route::get('/auth/login', [AdadController::class, 'login'])->name('login');
+
+Route::get('/auth/register', [AdadController::class, 'register'])->name('register');
+// ->middleware('auth');
+
+Route::get('forgot-password', [AdadController::class, 'showForgetPasswordForm'])->name('showForgetPasswordForm');
+
+// Autenticação
+Route::get('/logout', [AdadController::class, 'logout'])->name('logout');
+
+Route::get('reset_password/{token}', [AdadController::class, 'showResetPasswordForm'])->name('resetPassword');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
--- routes/web.php
+++ routes/web.php
@@ -5,49 +5,48 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdadController;
 use App\Http\Controllers\IgrejaController;
 
-
 // Rotas relativas as páginas da igreja Campo Belo
 Route::get('/', [IgrejaController::class, 'mainPg'])->name('main.page');
-Route::get('/institucional', [IgrejaController::class, 'institutionalPg'])->name('institutional.page');
-Route::get('/contato', [IgrejaController::class, 'form_create'])->name('contact.page');
-Route::get('/projetos', [IgrejaController::class, 'projectsPg'])->name('projects.page');
-Route::get('/reunioes', [IgrejaController::class, 'reunionsPg'])->name('reunions.page');
+
+Route::redirect('/institucional', 'api/institucional')->name('institutional.page');
+Route::redirect('/contato', 'api/contato')->name('contact.page');
+Route::redirect('/projetos', 'api/projetos')->name('projects.page');
+Route::redirect('/reunioes', 'api/reunioes')->name('reunions.page');
 
 Route::post('/sendForm', [IgrejaController::class, 'sendForm'])->name('form_send');
 
 // Rotas para o sistema de gestão do adad
-Route::get('/AreaRestrita', [AdadController::class, 'aluno_create'])
-->name('aluno_create')
-->middleware('auth');
+Route::get('/AreaRestrita', [AdadController::class, 'aluno_create'])->name('aluno_create');
+// ->middleware('auth');
 
 Route::post('/alunos', [AdadController::class, 'aluno_store'])->name('aluno_store');
 
 Route::delete('/alunos/{id}', [AdadController::class, 'aluno_destroy'])->name('aluno_destroy');
 
+
 Route::get('/alunos/edit/{id}', [AdadController::class, 'aluno_edit'])->name('aluno_edit');
+
 
 Route::put('/alunos/update/{id}', [AdadController::class, 'aluno_update'])->name('aluno_update');
 
-
 // Funções de Sistema de Login
 //  Login / Registrar (get)
-Route::get('/auth/login', [AdadController::class, 'login'])->name('login');
+Route::redirect('/auth/login_web', '/api/auth/login')->name('login');
 
-Route::get('/auth/register', [AdadController::class, "register"])->name('register');
-// ->middleware('auth');
+Route::redirect('/auth/register_web', '/api/auth/register')->name('register');
 
-Route::get('forgot-password', [AdadController::class, 'showForgetPasswordForm'])->name('showForgetPasswordForm');
+Route::redirect('forgot-password_web', '/api/forgot-password')->name('showForgetPasswordForm');
 
 // Login / Registrar (post)
-Route::post('/authenticate', [AdadController::class, "autorizar"])->name('authenticate');
+Route::post('/authenticate', [AdadController::class, 'autorizar'])->name('authenticate');
 
-Route::post('/newuser', [AdadController::class, "store"])->name('newuser');
+Route::post('/newuser', [AdadController::class, 'store'])->name('newuser');
 
 // Autenticação
-Route::get('/logout', [AdadController::class, 'logout'])->name('logout');
+Route::redirect('/logout_web', '/api/logout')->name('logout');
 
 Route::post('forget_password', [AdadController::class, 'submitForgetPasswordForm'])->name('submitForgetPasswordForm');
 
-Route::get('reset_password/{token}', [AdadController::class, 'showResetPasswordForm'])->name('resetPassword');
+Route::redirect('reset_password_web/{token}', '/api/reset_password/{token}')->name('resetPassword');
 
 Route::post('reset_password', [AdadController::class, 'submitResetPasswordForm'])->name('submitPassword');
